a6143af8fc11b4c9386cb29437535944e2a354b6
Mahed Ahmed Chowdhury <mahed255261@gmail.com>
2024-04-02 21:07:20 +0600

    home page table design

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,52 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div className="mh-100">
-      <h1 className="ms-5">this is home page</h1>
+    <div className="container home_page mt-5 d-flex justify-content-center">
+      <div>
+        <table className="table custom-table">
+          <thead>
+            <tr>
+              <th scope="col">Id</th>
+              <th scope="col">Name</th>
+              <th scope="col">Age</th>
+              <th scope="col">Last Visited</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>John Doefdghhhhhhh</td>
+              <td>30</td>
+              <td>2023-12-01</td>
+              <td>
+                <button className="btn btn-primary me-1">View Profile</button>
+                <button className="btn btn-success me-1">
+                  Previous Prescription
+                </button>
+                <button className="btn btn-info me-1">Add Prescription</button>
+                <button className="btn btn-danger">Delete</button>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">1</th>
+              <td>John Doefdghhhhhhh</td>
+              <td>30</td>
+              <td>2023-12-01</td>
+              <td>
+                <button className="btn btn-primary me-1">View Profile</button>
+                <button className="btn btn-success me-1">
+                  Previous Prescription
+                </button>
+                <button className="btn btn-info me-1">Add Prescription</button>
+                <button className="btn btn-danger">Delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
